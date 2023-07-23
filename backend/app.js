a79231a5f97b87b85a56fac0c751b42b77296964
v-tao <vivian0tao@gmail.com>
2023-07-23 11:46:39 -0400

    add pool connection

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const deckRoutes = require("./routes/deck");
     ideaRoutes = require("./routes/idea");
+const pool = require("./pool");
 
 app.use("/decks", deckRoutes);
 app.use("/ideas", ideaRoutes);
