7c9db00c6f2cef40d2cb3bf046e4bcf3413e261c
v-tao <vivian0tao@gmail.com>
2023-07-23 20:09:58 -0400

    CORS allow requests from localhost:4200

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,10 @@ app.use(express.json());
 app.use("/decks", deckRoutes);
 app.use("/ideas", ideaRoutes);
 
+app.use(cors({
+    origin: "http://localhost:4200"
+}));
+
 app.listen(3000, () => {
     console.log("Server started");
 })
