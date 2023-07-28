23eccce6da53b0a4ea64d04a5f65fd4457d13dc2
v-tao <vivian0tao@gmail.com>
2023-07-27 23:25:46 -0400

    add removeIdeaFromDeck

--- frontend/big-ideas/src/app/deck/deck.component.ts
+++ frontend/big-ideas/src/app/deck/deck.component.ts
@@ -3,6 +3,7 @@ import { DeckService } from './deck.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { Deck } from '../models/deck';
+import { Idea } from '../models/idea';
 
 @Component({
   selector: 'app-deck',
@@ -26,10 +27,24 @@ export class DeckComponent implements OnInit, OnDestroy {
       const id = +params.get('id')!;
       this.deckService.getDeckById(id).subscribe({
         next: (deck: Deck) => this.deck = deck,
-        error: (err) => console.error(err),
+        error: (err) => console.error(err)
       })
     });
-    this.subscriptions?.push(getDeckByIdSubscription);
+    this.subscriptions.push(getDeckByIdSubscription);
+  }
+
+  removeIdeaFromDeck(ideaId: number) {
+    if (this.deck) {
+      this.deck.ideas = this.deck.ideas.filter((idea: Idea) => idea.id != ideaId);
+    }
+    const removeIdeaFromDeckParamSubscription = this.route.paramMap.subscribe(params => {
+      const deckId = +params.get('id')!;
+      const removeIdeaFromDeckSubscription = this.deckService.removeIdeaFromDeck(deckId, ideaId).subscribe({
+        error: (err) => console.error(err)
+      })
+      this.subscriptions.push(removeIdeaFromDeckSubscription);
+    })
+    this.subscriptions.push(removeIdeaFromDeckParamSubscription);
   }
 
   deleteDeckById() {
@@ -37,9 +52,10 @@ export class DeckComponent implements OnInit, OnDestroy {
       const id = +params.get('id')!;
       this.deckService.deleteDeckById(id).subscribe({
         next: () => this.router.navigate(['/decks']),
-        error: (err) => console.error(err),
+        error: (err) => console.error(err)
       })
     })
+    this.subscriptions.push(deleteDeckByIdSubscription);
   }
 
   ngOnDestroy() {
--- frontend/big-ideas/src/app/deck/deck.service.ts
+++ frontend/big-ideas/src/app/deck/deck.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { Deck } from '../models/deck';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -14,6 +14,11 @@ export class DeckService {
   getDeckById(id: number): Observable<Deck> {
     const url = `${environment.apiUrl}/decks/${id}`;
     return this.http.get<Deck>(url);
+  }
+
+  removeIdeaFromDeck(deckId: number, ideaId: number): Observable<Deck> {
+    const url=`${environment.apiUrl}/decks/${deckId}/ideas/${ideaId}`;
+    return this.http.delete<Deck>(url);
   }
 
   deleteDeckById(id: number): Observable<Deck> {
